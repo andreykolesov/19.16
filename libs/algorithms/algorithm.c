edbc5ad0a091590d8f006942db2d86199bbee9b4
andreykolesov <1yasnoclown1@gmail.com>
2024-03-30 23:26:57 +0300

    countNonDescendingRowsMatrices (13 task)

--- libs/algorithms/algorithm.c
+++ libs/algorithms/algorithm.c
@@ -252,3 +252,29 @@ void swapPenultimateRow(matrix *m) {
 
     free(temp);
 }
+
+bool isNonDescendingSorted(int *a, int n) {
+    for (int i = 0; i < n - 1; i++)
+        if (a[i] > a[i + 1])
+            return false;
+
+    return true;
+}
+
+bool hasAllNonDescendingRows(matrix m) {
+    for (int i = 0; i < m.nRows; i++)
+        if (!isNonDescendingSorted(m.values[i], m.nCols))
+            return false;
+
+    return true;
+}
+
+int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
+    int amount_matrices = 0;
+
+    for (int i = 0; i < nMatrix; i++)
+        if (hasAllNonDescendingRows(ms[i]))
+            amount_matrices++;
+
+    return amount_matrices;
+}
